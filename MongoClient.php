a6387dc9538222899e05eb516ee608d1803aed3e
HeadScript <lostchaos3@gmail.com>
2015-12-19 14:14:21 +0100

    Release

--- MongoClient.php
+++ MongoClient.php
@@ -6,54 +6,70 @@
  * Date: 16.12.2015
  * Time: 18:51
  */
-class MongoClient
+use MongoDB\Client;
+
+class MongoClient extends Client
 {
-public function __construct ($server="mongodb://localhost:27017" , array $options = array("connect" => true), array $driver_options ) {
-
-}
-public  function close ($connection) {
-
-}
-    public function connect() {
-
-}
-    public function dropDB($db) {
+    public function __construct($server = "mongodb://localhost:27017", array $options = array("connect" => true))
+    {
+        parent::__construct($server,$options);
 
     }
-    public function __get($dbName) {
+
+    public function getConnections()
+    {
+        return $this->__debugInfo()["manager"]->getServers();
 
     }
-    public static function getConnections() {
+
+    public function getHosts()
+    {
 
     }
-    public function getHosts() {
+
+    public function getReadPreference()
+    {
 
     }
-    public function getReadPreference() {
+
+    public function getWriteConcern()
+    {
 
     }
-    public function getWriteConcern() {
+
+    public function killCursor($server_hash, $id)
+    {
 
     }
-    public function killCursor($server_hash,$id) {
+
+    public function listDBs()
+    {
 
     }
-    public function listDBs() {
+
+    public function selectCollection($db, $collection)
+    {
 
     }
-    public function selectCollection($db,$collection) {
+
+    public function selectDB($name)
+    {
+        return $this->selectDatabase($name);
 
     }
-    public function selectDB($name) {
+
+    public function setReadPreference($read_preference, $tags = null)
+    {
 
     }
-    public function setReadPreference($read_preference,$tags=null) {
+
+    public function setWriteConcern($w, $wtimeout = null)
+    {
 
     }
-    public function setWriteConcern($w,$wtimeout=null) {
 
-}
-    public function __toString() {
+    public function __toString()
+    {
         return "MongoClient";
     }
 
